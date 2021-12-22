709455bd6ff7f669f5596444bcd84ccc95fb9353
Adilet <berdibekovadilet@icloud.com>
2021-12-22 14:12:21 +0300

    Initial Commit

---
+++ Sushi/SushiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SushiApp.swift
+//  Sushi
+//
+//  Created by Adilet Berdibekov on 22.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct SushiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
